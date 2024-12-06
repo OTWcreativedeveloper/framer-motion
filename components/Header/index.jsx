49d0906acf46ae9a6b7c7531cfd1a956ba8947c4
muliadev2 <softhouse2@themulia.com>
2024-12-06 16:37:34 +0800

    fix menu responsive

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,30 +1,36 @@
 'use client';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion';
 import Button from './Button';
 import styles from './style.module.scss';
 import Nav from './Nav';
 
-const menu = {
-    open: {
-        width: "480px",
-        height: "650px",
-        top: "-25px",
-        right: "-25px",
-        transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
-    },
-    closed: {
-        width: "100px",
-        height: "40px",
-        top: "0px",
-        right: "0px",
-        transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1]}
-    }
-}
+
 
 export default function Index() {
     const [isActive, setIsActive] = useState(false);
-
+    const [width, setWidth] = useState(window.innerWidth);
+    const menu = {
+        open: {
+            width: width > 480 ? "480px" : "250px",
+            height:width > 480 ? "650px" : "450px",
+            top: "-25px",
+            right: "-25px",
+            transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
+        },
+        closed: {
+            width: "100px",
+            height: "40px",
+            top: "0px",
+            right: "0px",
+            transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1]}
+        }
+    }
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
     return (
         <div className={styles.header}>
             <motion.div 
